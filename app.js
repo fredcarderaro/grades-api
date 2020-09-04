009e38cd30b041ff1d552691ea95f773215fc15c
fredCarderaro <fredcarderaro@gmail.com>
2020-09-03 22:12:39 -0300

    Testando alteraçao no core origin url

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://grades-app-fc.herokuapp.com/',
+    origin: 'https://grades-app-fc.herokuapp.com',
   })
 );
 
